2d6e034aaf5c415540d9415909d0596a7195a5e6
SooaKim <alice2863@naver.com>
2022-11-12 02:49:37 +0900

    week1: expandable list layout

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -1,15 +1,35 @@
 package com.codelab.basics
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.codelab.basics.ui.theme.BasicsCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,9 +40,9 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MyApp()
                 }
             }
         }
@@ -30,14 +50,155 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+private fun MyApp(
+    modifier: Modifier = Modifier,
+
+) {
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+    Surface(
+        modifier = modifier,
+    ) {
+        if(shouldShowOnboarding){
+            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+        }else{
+            Greetings()
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+private fun Greetings(
+    modifier: Modifier = Modifier,
+    names: List<String> = List(1_000){ "$it" }
+){
+    Surface(
+        modifier = modifier.then(Modifier.padding(vertical = 4.dp)),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        LazyColumn {
+            items(names){ name ->
+                Greeting(name)
+            }
+        }
+    }
+}
+
+@Composable
+private fun OnboardingScreen(
+    onContinueClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Welcome to the Basics Codelab!")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked
+        ) {
+            Text("Continue")
+        }
+    }
+}
+
+@Composable
+private fun Greeting(name: String, modifier: Modifier = Modifier) {
+    var expanded by remember { mutableStateOf(false) }
+    val extraPadding by animateDpAsState(
+        if (expanded) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier.then(Modifier.padding(vertical = 4.dp, horizontal = 8.dp))
+    ) {
+        Row(
+            modifier = Modifier.padding(24.dp)
+        ) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
+                Text(text = "Hello, ")
+                Text(text = name, style = MaterialTheme.typography.headlineMedium.copy(
+                    fontWeight = FontWeight.ExtraBold
+                ))
+            }
+            ElevatedButton(onClick = { expanded = !expanded }) {
+                Text(if (expanded) "Show less" else "Show more")
+            }
+        }
+    }
+}
+
+@Preview(
+    name = "Light Mode",
+    showBackground = true,
+    widthDp = 320,
+    heightDp = 320,
+)
+@Preview(
+    name = "Dark Mode",
+    showBackground = true,
+    widthDp = 320,
+    heightDp = 320,
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+)
+@Composable
+private fun DefaultPreview() {
     BasicsCodelabTheme {
-        Greeting("Android")
+        Surface {
+            MyApp()
+        }
+    }
+}
+
+@Preview(
+    name = "Greetings Light Mode",
+    showBackground = true,
+    widthDp = 320,
+    heightDp = 320,
+)
+@Preview(
+    name = "Greetings Dark Mode",
+    showBackground = true,
+    widthDp = 320,
+    heightDp = 320,
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+)
+@Composable
+private fun GreetingsPreview() {
+    BasicsCodelabTheme {
+        Surface {
+            Greetings()
+        }
+    }
+}
+
+@Preview(
+    name = "Onboarding Light Mode",
+    showBackground = true,
+    widthDp = 320,
+    heightDp = 320,
+)
+@Preview(
+    name = "Onboarding Dark Mode",
+    showBackground = true,
+    widthDp = 320,
+    heightDp = 320,
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+)
+@Composable
+private fun OnboardingPreview() {
+    BasicsCodelabTheme {
+        Surface {
+            OnboardingScreen(onContinueClicked = {})
+        }
     }
 }
